8670d7f9aa516ddee3bb0184febdad3522200b6b
kamal9566 <kamalpotnuru195@gmail.com>
2026-03-17 09:03:57 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+class BookMyStayApp {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("     Welcome to Book My Stay App     ");
+        System.out.println("  Your Smart Hotel Booking System    ");
+        System.out.println("=====================================");
+
+        System.out.println("System is starting...");
+        System.out.println("Please choose an option to continue.");
     }
 }
